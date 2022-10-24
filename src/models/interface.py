68bff6a8fda915e8adbb6fe1579f4b487fb688f5
alexadam <paddze21@gmail.com>
2022-10-24 14:24:38 -0400

    Added model interface and basic MLP

---
+++ src/models/interface.py
@@ -0,0 +1,32 @@
+import abc
+
+import torch.nn.functional
+
+
+class ModelInterface(metaclass=abc.ABCMeta):
+    def __init__(self, *args, **kwargs):
+        self._threshold = 0.5
+        pass
+
+    @abc.abstractmethod
+    def forward(self, *args):
+        raise NotImplementedError
+
+    def predict(self, *args):
+        probs = self.predict_proba(*args)
+
+        return (probs[:, 1] > self.threshold).astype(int)
+
+    def predict_proba(self, *args):
+        with torch.no_grad():
+            output = self.forward(*args)
+
+        return torch.nn.functional.softmax(output, dim=1)
+
+    @property
+    def threshold(self):
+        return self._threshold
+
+    @threshold.setter
+    def threshold(self, new_threshold):
+        self._threshold = new_threshold
---
+++ src/models/mlp.py
@@ -0,0 +1,51 @@
+from .interface import ModelInterface
+import torch
+
+
+class MLP(torch.nn.Module, ModelInterface):
+    def __init__(self, input_dim, layers, activation):
+        super().__init__()
+
+        self.activation = getattr(torch.nn, activation)()
+
+        try:
+            self.layers = self._create_layers(layers, input_dim)
+        except Exception:
+            pass
+
+    def forward(self, x):
+        for i in range(len(self.layers) - 1):
+            x = self.layers[i](x)
+            x = self.activation(x)
+
+        x = self.layers[-1](x)
+
+        return x
+
+    def _create_layers(self, layers, input_dim):
+        if layers == 0:
+            fc = torch.nn.ModuleList([torch.nn.Linear(input_dim, 2)])
+        elif layers == 1:
+            fc = torch.nn.ModuleList([torch.nn.Linear(input_dim, 10),
+                                      torch.nn.Linear(10, 2)])
+        elif layers == 2:
+            fc = torch.nn.ModuleList([torch.nn.Linear(input_dim, 20),
+                                      torch.nn.Linear(20, 10),
+                                      torch.nn.Linear(10, 2)])
+        elif layers > 2:
+            initial_hidden_units = 50 * (2 ** layers)
+            fc = [torch.nn.Linear(input_dim, initial_hidden_units)]
+            prev_hidden_units = initial_hidden_units
+
+            for i in range(1, layers):
+                next_hidden_units = int(initial_hidden_units / (2 ** i))
+                fc.append(torch.nn.Linear(prev_hidden_units, next_hidden_units))
+
+                prev_hidden_units = next_hidden_units
+
+            fc.append(torch.nn.Linear(prev_hidden_units, 2))
+            fc = torch.nn.ModuleList(fc)
+        else:
+            raise Exception("Number of layers cannot be negative")
+
+        return fc
