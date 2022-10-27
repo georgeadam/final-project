446d083195a830fd8157fa75ab63619931362030
alexadam <paddze21@gmail.com>
2022-10-27 10:42:31 -0400

    Added negative bias noise corruptor

--- src/corruptors/__init__.py
+++ src/corruptors/__init__.py
@@ -1,5 +1,6 @@
 from .creation import corruptors
 from .error_amplification import ErrorAmplification
 from .error_oscillation import ErrorOscillation
+from .negative_bias_noise import NegativeBiasNoise
 from .positive_bias_noise import PositiveBiasNoise
 from .uniform_noise import UniformNoise
---
+++ src/corruptors/negative_bias_noise.py
@@ -0,0 +1,26 @@
+import copy
+
+import numpy as np
+
+from .corruptor import CorruptorInterface
+from .creation import corruptors
+
+
+class NegativeBiasNoise(CorruptorInterface):
+    def __init__(self, noise_level):
+        self.noise_level = noise_level
+
+    def corrupt(self, module, data_module, trainer, update_num):
+        update_batch_dataloader = data_module.current_update_batch_dataloader(update_num)
+        _, preds, y = trainer.predict(module, dataloaders=update_batch_dataloader)
+
+        new_y = copy.deepcopy(y)
+
+        positive_idx = np.where(preds == 1)[0]
+        noise_idx = np.random.choice(positive_idx, size=int(self.noise_level * len(positive_idx)), replace=False)
+        new_y[noise_idx] = 0
+
+        data_module.overwrite_current_update_labels(new_y, update_num)
+
+
+corruptors.register_builder("negative_bias_noise", NegativeBiasNoise)
