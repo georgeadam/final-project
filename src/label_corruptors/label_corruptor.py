580294266da697b9360f77e4b0c73541a2b09445
alexadam <paddze21@gmail.com>
2022-10-30 20:16:55 -0400

    Made label corruptor subsample work with less samples than sample_limit

--- src/label_corruptors/label_corruptor.py
+++ src/label_corruptors/label_corruptor.py
@@ -10,6 +10,6 @@ class LabelCorruptor:
 
     def subset_indices(self, indices, sample_limit):
         if sample_limit is not None and len(indices) > sample_limit:
-            indices = np.random_choice(indices, sample_limit, replace=False)
+            indices = np.random_choice(indices, min(len(indices), sample_limit), replace=False)
 
         return indices
