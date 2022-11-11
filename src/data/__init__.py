bb7385d3a490e7a89bfb6f617b34ca06e99bb207
alexadam <paddze21@gmail.com>
2022-11-10 21:42:07 -0500

    Added miniboone dataset

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -4,4 +4,5 @@ from .cifar10_tabular import CIFAR10Tabular
 from .creation import data_modules
 from .higgs import Higgs
 from .mimic_iv import MIMICIV
+from .miniboone import Miniboone
 from .numerai import Numerai
---
+++ src/data/miniboone.py
@@ -0,0 +1,65 @@
+import numpy as np
+import pandas as pd
+from scipy.io import arff
+from torchvision.transforms import Compose
+
+from .creation import data_modules
+from .data_module import DataModule
+from .feeders import feeders
+from .splitters import splitters
+from .transforms import transforms
+
+
+class Miniboone(DataModule):
+    def __init__(self, data_dir, batch_size, feeder_args, splitter_args):
+        super().__init__(data_dir, batch_size)
+
+        self._numeric_cols = None
+        self._feeder_args = feeder_args
+        self._splitter_args = splitter_args
+
+        self.setup(None)
+
+    def setup(self, stage=None):
+        if not self.data_feeder:
+            data = arff.loadarff(self.data_dir)[0]
+            data = pd.DataFrame(data)
+            data = data.dropna()
+
+            features = [c for c in data.columns if not c.startswith("signal")]
+            x = data[features]
+            y = pd.factorize(data["signal"])[0]
+
+            x = x.to_numpy().astype("float32")
+            y = y.astype("float32")
+            indices = np.arange(len(x))
+
+            splitter = splitters.create(self._splitter_args.name, **self._splitter_args.params)
+            splitted_data = splitter.split_data(x, y, indices)
+            self.data_feeder = feeders.create(self._feeder_args.name, **self._feeder_args.params,
+                                              splitted_data=splitted_data)
+            self._num_updates = self.data_feeder.num_updates
+            self._data_dimension = x.shape[1]
+
+            self.update_transforms(0)
+
+    def update_train_transform(self, x):
+        scaler = transforms.create("scaler", cols=None)
+        scaler.fit(x)
+
+        tensor = transforms.create("tensor")
+
+        self.train_transform = Compose([scaler, tensor])
+        self.train_target_transform = None
+
+    def update_inference_transform(self, x):
+        scaler = transforms.create("scaler", cols=None)
+        scaler.fit(x)
+
+        tensor = transforms.create("tensor")
+
+        self.inference_transform = Compose([scaler, tensor])
+        self.inference_target_transform = None
+
+
+data_modules.register_builder("miniboone", Miniboone)
