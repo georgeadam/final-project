1070b2957ce07f1445d67f8d720074541fd87692
alexadam <paddze21@gmail.com>
2022-11-13 19:14:08 -0500

    Added resnet50 model

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,3 +1,4 @@
 from .creation import models
 from .fcn import FCN
 from .mlp import MLP
+from .resnet import ResNet50
---
+++ src/models/resnet50.py
@@ -0,0 +1,45 @@
+import torchvision
+
+from .model import Model
+from .creation import models
+import torch
+
+
+class ResNet50(Model):
+    def __init__(self, warm_start, **kwargs):
+        super().__init__(warm_start)
+        resnet = torchvision.models.resnet50(pretrained=False, num_classes=1)
+
+        self.relu = resnet.relu
+        self.maxpool = resnet.maxpool
+        self.avgpool = resnet.avgpool
+
+        self.conv1 = resnet.conv1
+        self.bn1 = resnet.bn1
+
+        self.layer1 = resnet.layer1
+        self.layer2 = resnet.layer2
+        self.layer3 = resnet.layer3
+        self.layer4 = resnet.layer4
+
+        self.fc = resnet.fc
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+
+        return x
+
+
+models.register_builder("resnet50", ResNet50)
