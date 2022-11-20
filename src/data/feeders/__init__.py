74ac976f7c1e3a88e2157d5f7a4273780600f753
alexadam <paddze21@gmail.com>
2022-11-20 10:34:22 -0500

    Added one-shot  data feeder

--- src/data/feeders/__init__.py
+++ src/data/feeders/__init__.py
@@ -1,3 +1,4 @@
 from .creation import feeders
 from .cumulative_static import CumulativeStatic
 from .current_static import CurrentStatic
+from .one_shot_static import OneShotStatic
---
+++ src/data/feeders/one_shot_static.py
@@ -0,0 +1,18 @@
+from .batch_fetchers import NullBatchFetcher
+from .creation import feeders
+from .label_updaters import NullLabelUpdater
+from .static import Static
+
+
+class OneShotStatic(Static):
+    def __init__(self, splitted_data, val_percentage, random_state):
+        super().__init__(splitted_data, val_percentage, 0, random_state)
+
+        self._label_updater = NullLabelUpdater()
+        self._batch_fetcher = NullBatchFetcher
+
+    def _get_all_data_for_split(self, update_num):
+        return self.x_train, self.y_train, self.indices_train
+
+
+feeders.register_builder("one_shot_static", OneShotStatic)
