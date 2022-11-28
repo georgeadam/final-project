588e8edc154b2de695b8772e78d9746c670e8698
alexadam <paddze21@gmail.com>
2022-11-28 18:17:36 -0500

    Removed n_test paramter from random splitter
    -This was not really being used previously since
    n_train and n_update determine n_test

--- src/data/splitters/random.py
+++ src/data/splitters/random.py
@@ -5,26 +5,27 @@ from .splitter import SplitterInterface
 
 
 class Random(SplitterInterface):
-    def __init__(self, n_train, n_update, n_test, random_state):
+    def __init__(self, n_train, n_update, random_state):
         self.n_train = n_train
         self.n_update = n_update
-        self.n_test = n_test
         self.random_state = random_state
 
     def split_data(self, x, y, indices):
-        x_train, x_update, y_train, y_update, indices_train, indices_update = train_test_split(x, y, indices,
-                                                                                               train_size=self.n_train,
-                                                                                               test_size=self.n_update + self.n_test,
+        n_train, n_update = self._data_sizes_to_counts(self.n_train, self.n_update, len(x))
+        x_train, x_update, y_train, y_update, indices_train, indices_update = train_test_split(x,
+                                                                                               y,
+                                                                                               indices,
+                                                                                               train_size=n_train,
                                                                                                random_state=self.random_state)
 
         if self.n_update == 0:
             x_test, y_test, indices_test = x_update, y_update, indices_update
             x_update, y_update, indices_update = None, None, None
         else:
-            x_update, x_test, y_update, y_test, indices_update, indices_test = train_test_split(x_update, y_update,
+            x_update, x_test, y_update, y_test, indices_update, indices_test = train_test_split(x_update,
+                                                                                                y_update,
                                                                                                 indices_update,
-                                                                                                train_size=self.n_update,
-                                                                                                test_size=self.n_test,
+                                                                                                train_size=n_update,
                                                                                                 random_state=self.random_state)
 
         splitted = {"x_train": x_train, "y_train": y_train, "indices_train": indices_train,
@@ -32,6 +33,15 @@ class Random(SplitterInterface):
                     "x_test": x_test, "y_test": y_test, "indices_test": indices_test}
 
         return splitted
+
+    def _data_sizes_to_counts(self, n_train, n_update, total):
+        if not isinstance(n_train, int):
+            n_train = int(n_train * total)
+
+        if not isinstance(n_update, int):
+            n_update = int(n_update * total)
+
+        return n_train, n_update
 
 
 splitters.register_builder("random", Random)
